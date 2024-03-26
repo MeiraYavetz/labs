3d20533e1cc88aa9463fe0f47df34d546ae0c4da
MeiraYavetz <121660709+MeiraYavetz@users.noreply.github.com>
2024-03-26 12:17:20 +0000

    works

--- src/Wallet/Wallet.sol
+++ src/Wallet/Wallet.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.12 <0.9.0;
+import "forge-std/console.sol";
 
 contract Wallet {
     address payable public owner;
@@ -13,6 +15,7 @@ contract Wallet {
         gabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] = 1;
     }
     modifier onlyOwner(){
+        console.log(owner);
         require(
             owner == msg.sender ,
             "Only the owner and the gabaim allowed to withdraw Ether");
@@ -21,6 +24,7 @@ contract Wallet {
     receive() external payable {}
 
     function withdraw(uint256 wad) external {
+        console.log(msg.sender);
         require(
             owner == msg.sender || gabaim[msg.sender] == 1,
             "Only the owner and the gabaim allowed to withdraw Ether"
@@ -33,11 +37,12 @@ contract Wallet {
     }
 
     function update(address oldGabai, address newGabai) public onlyOwner{
+    
     //    require(owner == msg.sender, "Only the owner can update"); //only owner can update gabaaim
-        require(gabaim[newGabai]==1,"the gabai is exist"); // check if gabbai exist in my hash
+        require(gabaim[oldGabai]==1,"the old gabai is not exist"); // check if gabbai exist in my hash
+        require(gabaim[newGabai]==0,"the gabai is exist"); // check if gabbai exist in my hash
         gabaim[newGabai]=1;
         gabaim[oldGabai]=0;
-
     }
 
     function getBalance() public view returns (uint256) {
--- test/Wallet/testWallet.t.sol
+++ test/Wallet/testWallet.t.sol
@@ -12,6 +12,7 @@ contract WalletTest is Test{
     Wallet public W;
     function  setUp() public{
         W = new Wallet();
+        payable(address(W)).transfer(1000); // move 1000 to the contract
     }
        function testReceive() public {
         address randomAddress = vm.addr(1234); // create random address
@@ -19,7 +20,7 @@ contract WalletTest is Test{
         uint256 amount = 1000;
         vm.deal(randomAddress, amount); // put money in this wallet
         uint256 initialBalance = address(W).balance; // the balance in the begining (before transfer)
-        payable(address(W)).transfer(1000); // move 50 to the contract
+        payable(address(W)).transfer(1000); // move 1000 to the contract
         uint256 finalBalance = address(W).balance; // the balance in the final (aftere transfer)
         assertEq(finalBalance, initialBalance + amount);
         vm.stopPrank();
@@ -31,10 +32,16 @@ contract WalletTest is Test{
         vm.startPrank(userAddress); // send from random address
         
         uint256 initialBalance = address(W).balance; // the balance in the begining (before transfer)
-        vm.expectRevert();
+        console.log(initialBalance);
+        console.log(withdrawAmount);
+        //vm.expectRevert();
+
         W.withdraw(withdrawAmount);
+
         uint256 finalBalance = address(W).balance; // the balance in the final (aftere transfer)
-        //assertEq(finalBalance, initialBalance - withdrawAmount);
+        console.log(finalBalance);
+        
+        assertEq(finalBalance, initialBalance - withdrawAmount);
         
         vm.stopPrank();
     }
@@ -46,29 +53,36 @@ contract WalletTest is Test{
         vm.startPrank(userAddress); // send from random address
         
         uint256 initialBalance = address(W).balance; // the balance in the begining (before transfer)
+        console.log(initialBalance);
+        console.log(withdrawAmount);
         vm.expectRevert();
         W.withdraw(withdrawAmount);
         uint256 finalBalance = address(W).balance; // the balance in the final (aftere transfer)
-        //assertEq(finalBalance, initialBalance - withdrawAmount);
+        console.log(finalBalance);
+
+        assertEq(finalBalance, initialBalance - withdrawAmount);
         
         vm.stopPrank();
     }
     function testUpdate() public{
         address oldGabai = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
-        address newGabai = 0x7c0FA5571c4A1A67FD21Ed9209674868cC8dc86b;
+        address newGabai = 0x57C91e4803E3bF32c42a0e8579aCaa5f3762af71;
 
-        address userAddress = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d; // address of allowed user
+        address userAddress = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496; // address of allowed user
+        
         //address userAddress = vm.addr(1); // address of not allowed user
         vm.startPrank(userAddress); // send from random address
+        
+        W.update(oldGabai, newGabai);
+        
         vm.expectRevert();
         W.update(oldGabai, newGabai);
         
-       
-        
-        //assertEq(W.gabaim(newGabai),1);
-        //assertEq(W.gabaim(oldGabai),0);
+        assertEq(W.gabaim(newGabai),1);
+        assertEq(W.gabaim(oldGabai),0);
+        assertEq(W.owner(),0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496); 
         vm.stopPrank();
-        //assertEq(W.owner(),0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d); 
+
     }
     function testGetBalance() public{
        
