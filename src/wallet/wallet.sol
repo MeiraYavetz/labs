20d321d8afbf80b4b567f088037b3a3146cfe745
MeiraYavetz <mayalea6745@gmail.com>
2024-06-03 16:59:32 +0300

    Proxy

--- src/wallet/wallet.sol
+++ src/wallet/wallet.sol
@@ -1,16 +1,51 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.20;
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.12 <0.9.0;
+import "forge-std/console.sol";
 
 contract Wallet {
     address payable public owner;
 
+    mapping(address => uint256) public gabaim;
+
     constructor() {
         owner = payable(msg.sender);
+        gabaim[0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d] = 1;
+        gabaim[0x7c0FA5571c4A1A67FD21Ed9209674868cC8dc86b] = 1;
+        gabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] = 1;
     }
-
+    modifier onlyOwner(){
+        console.log(owner);
+        require(
+            owner == msg.sender ,
+            "Only the owner and the gabaim allowed to withdraw Ether");
+            _;
+    }
     receive() external payable {}
 
-    function withdraw(uint256 amount) public {
-        owner.transfer(amount);
+    function withdraw(uint256 wad) external {
+        console.log(msg.sender);
+        require(
+            owner == msg.sender || gabaim[msg.sender] == 1,
+            "Only the owner and the gabaim allowed to withdraw Ether"
+        );
+        /*require(
+            address(this).balance >= wad,
+            "there is no money"
+        );*/
+        payable(msg.sender).transfer(wad);
+    }
+
+    function update(address oldGabai, address newGabai) public onlyOwner{
+    
+    //    require(owner == msg.sender, "Only the owner can update"); //only owner can update gabaaim
+        require(gabaim[oldGabai]==1,"the old gabai is not exist"); // check if gabbai exist in my hash
+        require(gabaim[newGabai]==0,"the gabai is exist"); // check if gabbai exist in my hash
+        gabaim[newGabai]=1;
+        gabaim[oldGabai]=0;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
