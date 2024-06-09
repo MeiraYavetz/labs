d98d70c7e7873b5fdfcb557b8d75c5e5e551d510
MeiraYavetz <mayalea6745@gmail.com>
2024-06-09 16:16:43 +0300

    commit

--- src/AMM/Amm1.sol
+++ src/AMM/Amm1.sol
@@ -3,118 +3,118 @@
 // Code is a stripped down version of Synthetix
 
 pragma solidity ^0.8.20;
-import "../like/IERC20.sol";
-import "forge-std/console.sol";
-import "../audit/approve.sol";
-import "../Staking/MyToken.sol";
-import "../Staking/MyToken2.sol";
-contract Amm1{
+// import "../like/IERC20.sol";
+// import "forge-std/console.sol";
+// import "../audit/approve.sol";
+// import "../Staking/MyToken.sol";
+// import "../Staking/MyToken2.sol";
+// contract Amm1{
     
-    struct tokens{
-        uint tX;
-        uint tY;
-    }
+//     struct tokens{
+//         uint tX;
+//         uint tY;
+//     }
 
-    MyToken x;
-    MyToken2 y;
+//     MyToken x;
+//     MyToken2 y;
 
-    uint balanceX;
-    uint balanceY;
+//     uint balanceX;
+//     uint balanceY;
 
-    uint totalSupply;
+//     uint totalSupply;
     
-    uint WAD;
+//     uint WAD;
 
-    mapping(address => tokens) public users; 
+//     mapping(address => tokens) public users; 
 
-    constructor(address _tokenX, address _tokenY){
-        //x = IERC20(_tokenX);
-        //y = IERC20(_tokenY);
+//     constructor(address _tokenX, address _tokenY){
+//         //x = IERC20(_tokenX);
+//         //y = IERC20(_tokenY);
 
-        x = new MyToken();
-        y = new MyToken2();
+//         x = new MyToken();
+//         y = new MyToken2();
 
-        x.approve(address(this),100); 
-        y.approve(address(this),100);
-        x.mint(100);
-        y.mint(100);
+//         x.approve(address(this),100); 
+//         y.approve(address(this),100);
+//         x.mint(100);
+//         y.mint(100);
 
-        _mint(100,100);
-        WAD = 10**18;
-    }
+//         _mint(100,100);
+//         WAD = 10**18;
+//     }
  
-    function _mint(uint256 amountX, uint256 amountY) private {
-        balanceX += amountX;
-        balanceY += amountY;
-        totalSupply = balanceX + balanceY;
-    }
+//     function _mint(uint256 amountX, uint256 amountY) private {
+//         balanceX += amountX;
+//         balanceY += amountY;
+//         totalSupply = balanceX + balanceY;
+//     }
 
-    function _burn(uint256 amountX, uint256 amountY) private {
-        balanceX -= amountX;
-        balanceY -= amountY;
-        totalSupply = balanceX + balanceY;
-    }
-    function price() public view returns(uint){
-        return balanceX > balanceY ? (balanceX * WAD / balanceY) : (balanceY * WAD /balanceX);
-    }
+//     function _burn(uint256 amountX, uint256 amountY) private {
+//         balanceX -= amountX;
+//         balanceY -= amountY;
+//         totalSupply = balanceX + balanceY;
+//     }
+//     function price() public view returns(uint){
+//         return balanceX > balanceY ? (balanceX * WAD / balanceY) : (balanceY * WAD /balanceX);
+//     }
  
-    function tradeXToY(uint256 amount) public returns(uint){
-        require(amount > 0, "amount is illegal");
-        // x.approve(address(this),100);
-        // x.approve(msg.sender,100);
-        console.log("msg.sender in amm1",msg.sender);
-        console.log(msg.sender, "msg.sender"); 
-        console.log(x.balanceOf(msg.sender), "balanceOf.msg.sender");  
-        x.transferFrom(msg.sender,address(this),amount);
-        balanceX += amount;
-        uint amountY = balanceY * WAD / price();
-        console.log(amountY, "amountY");  
-        uint result = balanceY - amountY;  
-        console.log(result, "result");  
-        require(result < balanceY, "There is no enough liquidity");
-        console.log("dd",y.balanceOf(address(this)));
-        y.transfer(msg.sender,result);
-        balanceY -= result;
-        return result;
-    }
+//     function tradeXToY(uint256 amount) public returns(uint){
+//         require(amount > 0, "amount is illegal");
+//         // x.approve(address(this),100);
+//         // x.approve(msg.sender,100);
+//         console.log("msg.sender in amm1",msg.sender);
+//         console.log(msg.sender, "msg.sender"); 
+//         console.log(x.balanceOf(msg.sender), "balanceOf.msg.sender");  
+//         x.transferFrom(msg.sender,address(this),amount);
+//         balanceX += amount;
+//         uint amountY = balanceY * WAD / price();
+//         console.log(amountY, "amountY");  
+//         uint result = balanceY - amountY;  
+//         console.log(result, "result");  
+//         require(result < balanceY, "There is no enough liquidity");
+//         console.log("dd",y.balanceOf(address(this)));
+//         y.transfer(msg.sender,result);
+//         balanceY -= result;
+//         return result;
+//     }
 
-    function tradeYToX(uint amount) public returns(uint){
-        require(amount > 0, "amount is illegal");
-        y.transferFrom(address(msg.sender),address(this),amount);
-        balanceY += amount;
-        uint amountX =balanceX * WAD / price();
-        uint result =balanceX - amountX;
-        require(result <balanceX, "There is no enough liquidity");
-        x.transfer(msg.sender,result);
-        balanceX -= result;
-        return result;
-    }
+//     function tradeYToX(uint amount) public returns(uint){
+//         require(amount > 0, "amount is illegal");
+//         y.transferFrom(address(msg.sender),address(this),amount);
+//         balanceY += amount;
+//         uint amountX =balanceX * WAD / price();
+//         uint result =balanceX - amountX;
+//         require(result <balanceX, "There is no enough liquidity");
+//         x.transfer(msg.sender,result);
+//         balanceX -= result;
+//         return result;
+//     }
 
-    function addLiquidity(uint amountX, uint amountY) public { //->amount
-        uint rate = amountX > amountY ? (amountX * WAD / amountY) : (amountY * WAD / amountX);
-        require(rate == price(), "rate not equal");
-        x.transferFrom(address(msg.sender),address(this),amountX);
-        balanceX += amountX;
-        y.transferFrom(address(msg.sender),address(this),amountY);
-        balanceY += amountY;
-        totalSupply = balanceX + balanceY;
-        users[msg.sender].tX += amountX;
-        users[msg.sender].tY += amountY;
-    }
+//     function addLiquidity(uint amountX, uint amountY) public { //->amount
+//         uint rate = amountX > amountY ? (amountX * WAD / amountY) : (amountY * WAD / amountX);
+//         require(rate == price(), "rate not equal");
+//         x.transferFrom(address(msg.sender),address(this),amountX);
+//         balanceX += amountX;
+//         y.transferFrom(address(msg.sender),address(this),amountY);
+//         balanceY += amountY;
+//         totalSupply = balanceX + balanceY;
+//         users[msg.sender].tX += amountX;
+//         users[msg.sender].tY += amountY;
+//     }
 
-    function removeLiquidity(uint amountX, uint amountY) public { //->amount
-        uint rate = amountX > amountY ? (amountX * WAD / amountY) : (amountY * WAD / amountX);
-        require(rate == price(), "rate not equal");
-        require(amountX <= balanceX, "There is no enough token X");
-        require(amountY <= balanceY, "There is no enough token Y");
-        require(amountX <= users[msg.sender].tX, "You don't have enough token Y");
-        require(amountY <= users[msg.sender].tY, "You don't have enough token Y");
-        x.transfer(address(msg.sender),amountX);
-        balanceX -= amountX;
-        y.transfer(address(msg.sender),amountY);
-        balanceY -= amountY;
-        totalSupply = balanceX + balanceY;
-        users[msg.sender].tX -= amountX;
-        users[msg.sender].tY -= amountY;
-    }
-}
+//     function removeLiquidity(uint amountX, uint amountY) public { //->amount
+//         uint rate = amountX > amountY ? (amountX * WAD / amountY) : (amountY * WAD / amountX);
+//         require(rate == price(), "rate not equal");
+//         require(amountX <= balanceX, "There is no enough token X");
+//         require(amountY <= balanceY, "There is no enough token Y");
+//         require(amountX <= users[msg.sender].tX, "You don't have enough token Y");
+//         require(amountY <= users[msg.sender].tY, "You don't have enough token Y");
+//         x.transfer(address(msg.sender),amountX);
+//         balanceX -= amountX;
+//         y.transfer(address(msg.sender),amountY);
+//         balanceY -= amountY;
+//         totalSupply = balanceX + balanceY;
+//         users[msg.sender].tX -= amountX;
+//         users[msg.sender].tY -= amountY;
+//     }
+// }
--- src/AMM/cp.sol
+++ src/AMM/cp.sol
@@ -1,78 +1,125 @@
 // SPDX-License-Identifier: MIT
+// https://solidity-by-example.org/defi/staking-rewards/
+// Code is a stripped down version of Synthetix
+
 pragma solidity ^0.8.20;
+
 import "../like/IERC20.sol";
+import "forge-std/console.sol";
+import "../audit/approve.sol";
 
-contract CP{
-    IERC20 public immutable token1;
-    IERC20 public immutable token2;
+contract CpAmm {
+    IERC20 public immutable tokenA;
+    IERC20 public immutable tokenB;
 
-    uint public reserve1;
-    uint public reserve2;
+    uint256 public reserveA; // amount of tokenA
+    uint256 public reserveB; // amount of tokenB
 
-    uint public totalSupply;
+    uint256 public totalSupply; // amount of shares
+    uint256 WAD = 10 ** 18;
 
-    mapping(address => uint) public balances;
+    mapping(address => uint256) public balances; // shares per user
 
-    constructor(address t1,address t2){
-        token1 = IERC20(t1);
-        token2 = IERC20(t2);
+    constructor(address tA, address tB) {
+        tokenA = IERC20(tA); // type of tokenA
+        tokenB = IERC20(tB); // type of tokenB
     }
-    function mint(address to,uint amount) private{
+
+    function mint(address to, uint256 amount) private {
         balances[to] += amount;
         totalSupply += amount;
     }
 
-    function burn(address from, uint amount) private{
+    function burn(address from, uint256 amount) private {
         balances[from] -= amount;
         totalSupply -= amount;
     }
 
-    function swap(address addIn, uint amountIn) external
-    returns (uint amountOut){
-        require(
-            addIn == address(token1) || addIn == address(token2),
-            "AMM3-invalid-token"
-        );
-        require(amountIn > 0, "AMM3-zero-amount");
+    function swap(address addToken, uint256 amountIn) external returns (uint256 amountOut) {
+        require(addToken == address(tokenA) || addToken == address(tokenB), "AMM-invalid-token");
+        require(amountIn > 0, "AMM-zero-amount");
 
-        bool isToken1 = addIn == address(token1);
-        (IERC20 tokenIn, IERC20 tokenOut, uint reserveIn, uint reserveOut)
-        = isToken1
-            ? (token1,token2,reserve1,reserve2)
-            : (token2,token1,reserve2,reserve1);
-        tokenIn.transferFrom(msg.sender,address(this),amountIn);
+        bool isTokenA = addToken == address(tokenA); // check if addToken is tokenA
 
-        uint amountInWithFee = (amountIn * 997) / 1000;
-        amountOut = (reserveOut * amountInWithFee) /
-                    (reserveIn + amountInWithFee);
-        
-        tokenOut.transfer(msg.sender,amountOut);
+        // initialize the variables by the type of user token;
+        (IERC20 tokenIn, IERC20 tokenOut, uint256 reserveIn, uint256 reserveOut) =
+            isTokenA ? (tokenA, tokenB, reserveA, reserveB) : (tokenB, tokenA, reserveB, reserveA);
 
-        reserve1 = token1.balanceOf(address(this));
-        reserve2 = token2.balanceOf(address(this));
+        tokenIn.transferFrom(msg.sender, address(this), amountIn); // transfer the tokens (user want to swap) from user to poll
+
+        // 0.3% fee for swaping
+        uint256 amountInWithFee = (amountIn * 997) / 1000;
+        amountOut = (amountInWithFee * reserveOut) / (reserveIn + amountInWithFee);
+
+        tokenOut.transfer(msg.sender, amountOut); // transfer the tokens (after swap and fee) from poll to user
+
+        reserveA = tokenA.balanceOf(address(this));
+        reserveB = tokenB.balanceOf(address(this));
     }
 
-    function addLiquidity(uint amount1, uint amount2) external
-    returns (uint shares) {
-        token1.transferFrom(msg.sender,address(this),amount1);
-        token2.transferFrom(msg.sender,address(this),amount2);
-
-        if(reserve1 > 0 || reserve2 > 0){
-            require(reserve1 * amount2 == reserve2 * amount1,
-            "x/y != dx/dy");
+    function addLiquidity(uint256 amountA, uint256 amountB) external returns (uint256 shares) {
+        // when reserveA and reserveB equal 0 (in the first time - empty pool) we will not check the rate - no rate
+        if (reserveA > 0 || reserveB > 0) {
+            // reserve - before the change, amount - dx, dy
+            require(reserveA * amountA == reserveB * amountB, "x/y !=dx/dy"); // check the rate of the pool
         }
-        if(totalSupply == 0){
-            shares = sqrt(amount1 * amount2);
-        }else{
-            shares = min(
-                (amount1 * totalSupply) / reserve1,
-                (amount2 * totalSupply) / reserve2
-            );
-        }
-        require(shares > 0,"shares = 0");
-        mint(msg.sender,shares);
 
-        reserve1 = token1.balanceOf(address(this));
-        reserve2 = token2.balanceOf(address(this));
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        if (totalSupply == 0) {
+            shares = sqrt(amountA * amountB); // share = sqrt(k)
+        } else {
+            shares = min((amountA * totalSupply) / reserveA, (amountB * totalSupply) / reserveB);
+        }
+        require(shares > 0, "shares = 0"); // when amountA or AmountB eq 0 the shares will be 0 because the multiply
+        mint(msg.sender, shares); // update share for user
+
+        reserveA = tokenA.balanceOf(address(this)); // update amount of tokenA
+        reserveB = tokenB.balanceOf(address(this)); // update amount of tokenB
     }
+
+    function removeLiquidity(uint256 _shares) external returns (uint256 amountA, uint256 amountB) {
+        require(_shares <= balances[msg.sender], "you don't have this amount of shares");
+        uint256 balA = tokenA.balanceOf(address(this));
+        uint256 balB = tokenB.balanceOf(address(this));
+
+        // save the ratio of the pool
+        amountA = (_shares * balA) / totalSupply;
+        amountB = (_shares * balB) / totalSupply;
+        require(amountA > 0 && amountB > 0, "amountA or amountB = 0");
+
+        burn(msg.sender, _shares); // update share for user
+
+        // update amount of tokens
+        reserveA = balA - amountA;
+        reserveB = balB - amountB;
+
+        // transfer amount of tokens to user
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
+    }
+
+    function sqrt(uint256 y) private pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+
+    //AMM
+
+    // advantage:
+
+    // disadvantage:
 }
