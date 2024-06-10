3f76e6c9e7121fa6c5d28b283431438464e340db
MeiraYavetz <mayalea6745@gmail.com>
2024-06-10 16:50:59 +0300

    tests

---
+++ src/DeFiVulnLabsTests/Array-deletionTest.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract DeletionInArrayTest is Test{
+
+
+}
+contract ArrayDeletion {
+    uint[] public arr = [1,2,3,4,5];
+    function getLength() public view returns(uint length){
+        
+    }
+}
